4d0dd133b011e14c7c24f6a0767067de8a36fb0d
CristianS <cristianserrano79@gmail.com>
2024-08-07 11:10:50 -0300

    update

--- src/dataBase/dataBase.js
+++ src/dataBase/dataBase.js
@@ -3,6 +3,7 @@ import mysql2 from 'mysql2/promise';
 const newConnection = async () => {
     const connection = await mysql2.createConnection({
         host: "localhost",
+        port: 3306,
         user: "root",
         database: "tasks_db",
     });
